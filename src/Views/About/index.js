b240c44fb3dd0b6a7d107eb3f9bb875ed139d6e1
peter <pete.bryant@gmail.com>
2020-12-17 22:02:16 +0000

    async example on the about page

--- src/Views/About/index.js
+++ src/Views/About/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Async from 'react-async';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid'
 import { makeStyles } from '@material-ui/core/styles';
 
 var pjson = require('../../../package.json');
 
+const loadJsonAsync = async ({id}) => {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
+  if (!response.ok) throw new Error(response.statusText)
+
+  return response.json()
+}
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -25,10 +32,21 @@ const useStyles = makeStyles((theme) => ({
           justify="center"
           style={{ minHeight: '100vh' }}
         >
-            <Grid item xs={4} alignItems="center" justify="center">
-                  <Typography variant="body1" gutterBottom align='center'>
-                    All rights reserved © 2020 liminal - Version {pjson.version}
-                  </Typography>
+            <Grid item xs={4}>
+              <Async promiseFn={loadJsonAsync} id={1}>
+                <Typography variant="body1" gutterBottom align='center'>
+                  <Async.Pending>Loading...</Async.Pending>
+                  <Async.Rejected>
+                    {error => `Something went wrong: ${error.message}`}
+                  </Async.Rejected>
+                  <Async.Fulfilled>
+                    {data => (<div>{data.title}</div>)}
+                  </Async.Fulfilled>                  
+                </Typography>
+              </Async>     
+              <Typography variant="body1" gutterBottom align='center'>
+                All rights reserved © 2020 liminal - Version {pjson.version}
+              </Typography>
             </Grid>  
           </Grid>
       </div>
