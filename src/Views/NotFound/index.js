210749b627c4a831027cfdf3458f9674b8b54aaa
peter <pete.bryant@gmail.com>
2020-12-19 19:24:57 +0000

    update 404 page

--- src/Views/NotFound/index.js
+++ src/Views/NotFound/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid'
 
 
 const useStyles = makeStyles((theme) => ({
@@ -13,7 +15,23 @@ const useStyles = makeStyles((theme) => ({
   
     return (
       <div className={classes.root}>      
-        Not Found....
+        <Grid
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justify="center"
+          style={{ minHeight: '100vh' }}
+        >
+            <Grid item xs={4}>   
+              <Typography variant="h1" gutterBottom align='center'>
+                404 :(
+              </Typography>
+              <Typography variant="h5" gutterBottom align='center'>
+                This is not the page you are looking for!
+              </Typography>
+            </Grid>  
+          </Grid>
       </div>
     );
   }
