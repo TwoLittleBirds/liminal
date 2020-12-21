f1682da6b26b0e5958770442bdc945ce08c6d602
peter <pete.bryant@gmail.com>
2020-12-21 21:13:00 +0000

    add ctor and componedntDidMount

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import  MainLayout from './Components/MainLayout';
 import { AppConfigurationClient } from "@azure/app-configuration";
 import { FlagsProvider } from 'react-feature-flags';
@@ -7,33 +7,42 @@ const client = new AppConfigurationClient(
   'Endpoint=https://reactfeatureflags.azconfig.io;Id=rbPk-lw-s0:0M5+JsBX32eVrLyi4opr;Secret=/SUxxNQpQfWS560BKlTTM3eQErrIA5nHW6B69M+T/yI='
 );
 
-async function getSettings() {
-  const settings = await client.listConfigurationSettings({labelFilter: "featureFlag"});
-  let flags = [];
+let _isMounted = false;
 
-  for await (const setting of settings) {
-    const jsonObj = JSON.parse(setting.value);
-    const obj = {name:jsonObj.id, isActive:jsonObj.enabled};
-    flags.push(obj);
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {flags: []};
+
   }
 
-  return flags;
-}
-
-let flags = [];
-export default function App() {
-  getSettings().then(data => { 
-    var length = data.length;
-    for (var i = 0; i < length; i++) {
-      flags.push(data[i]);
+  async componentDidMount(){
+    _isMounted = true;
+    const settings = client.listConfigurationSettings({labelFilter: "featureFlag"});
+    let flags = [];
+  
+    for await (const setting of settings) {
+      const jsonObj = JSON.parse(setting.value);
+      const obj = {name:jsonObj.id, isActive:jsonObj.enabled};
+      flags.push(obj);
     }
-  });
 
-  return (
-    <FlagsProvider value={flags}>
-      <MainLayout/>
-    </FlagsProvider>
-  );
+    if (_isMounted) {
+      this.setState({ flags })
+    }
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+
+  render(){
+    return(
+      <FlagsProvider value={this.state.flags}>
+        <MainLayout/>
+      </FlagsProvider>
+    )
+  }
 }
 
 
