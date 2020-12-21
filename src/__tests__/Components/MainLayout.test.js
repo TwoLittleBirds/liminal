3ec83a9cbf538464af6fc54ffafe03d36015180f
peter <pete.bryant@gmail.com>
2020-12-21 19:58:47 +0000

    fix tests for feature flag

--- src/__tests__/Components/MainLayout.test.js
+++ src/__tests__/Components/MainLayout.test.js
@@ -1,61 +1,89 @@
 import { render, screen } from '@testing-library/react';
 import MainLayout from '../../Components/MainLayout';
+import { FlagsProvider } from 'react-feature-flags';
 
+const flags = [{name: "AdminOnly", isActive: true}]
 
 test('renders appbar', () => {
-  render(<MainLayout />);
+  render(<FlagsProvider value={flags}>
+    <MainLayout/>
+  </FlagsProvider>);
 
   const elem = screen.getByTestId("menu-appbar");
   expect(elem).toBeInTheDocument();
 });
 
 test('renders toolbar', () => {
-  render(<MainLayout />);
+  render(<FlagsProvider value={flags}>
+    <MainLayout/>
+  </FlagsProvider>);
 
   const elem = screen.getByTestId("menu-toolbar");
   expect(elem).toBeInTheDocument();
 });
 
 test('renders iconbutton open', () => {
-    render(<MainLayout />);
+  render(<FlagsProvider value={flags}>
+    <MainLayout/>
+  </FlagsProvider>);
   
     const elem = screen.getByTestId("menu-iconopen");
     expect(elem).toBeInTheDocument();
 });
 
 test('renders iconbutton close', () => {
-  render(<MainLayout />);
+  render(<FlagsProvider value={flags}>
+    <MainLayout/>
+  </FlagsProvider>);
 
   const elem = screen.getByTestId("menu-iconclose");
   expect(elem).toBeInTheDocument();
 });
 
 test('renders heading', () => {
-  render(<MainLayout />);
+  render(<FlagsProvider value={flags}>
+    <MainLayout/>
+  </FlagsProvider>);
 
   const elem = screen.getByTestId("menu-heading");
   expect(elem).toBeInTheDocument();
 });
 
 test('renders drawer', () => {
-  render(<MainLayout />);
+  render(<FlagsProvider value={flags}>
+    <MainLayout/>
+  </FlagsProvider>);
 
   const elem = screen.getByTestId("menu-drawer");
   expect(elem).toBeInTheDocument();
 });
 
 test('renders divider', () => {
-  render(<MainLayout />);
+  render(<FlagsProvider value={flags}>
+    <MainLayout/>
+  </FlagsProvider>);
 
   const elem = screen.getByTestId("menu-divider");
   expect(elem).toBeInTheDocument();
 });
 
 test('renders options', () => {
-  render(<MainLayout />);
+  render(<FlagsProvider value={flags}>
+    <MainLayout/>
+  </FlagsProvider>);
 
   const elem = screen.getByTestId("menu-options");
   expect(elem).toBeInTheDocument();
+});
+
+test('when not active does not render options', () => {
+  const flags = [{name: "AdminOnly", isActive: false}]
+
+  render(<FlagsProvider value={flags}>
+    <MainLayout/>
+  </FlagsProvider>);
+
+  expect(() => screen.getByTestId("menu-options")).toThrowError();
 });
 
 
